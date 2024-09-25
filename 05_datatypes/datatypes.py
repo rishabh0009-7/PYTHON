20b5933d944f5f654ac03e4027394531af753cde
rishabh0009-7 <rizzlifts2024@gmail.com>
2024-09-25 17:50:56 +0530

    Update datatypes.py

--- 05_datatypes/datatypes.py
+++ 05_datatypes/datatypes.py
@@ -1,1 +1,14 @@
-# dataypes or object types
+# dataypes or object types overview (what will we study )
+
+#number: 1234,3.1223,3+4j,0b111,decimal(),fraction()
+#string: 'span', "bib", b'a/x01c'
+# list:[1,[2,'three],4,5]  , list(range(10))  same as array in other lang
+#tuple: (1,'spam' ,4,'y'),  tuple('spam'), namedtuple
+#dictionary : {'food':'spam' , 'taste':'yum'} , dict(hours=10)
+#sets : set('abc') , {'a','b' , 'c'} // it contain unique elemnt inside
+#file: open('eggs.txt') , open(r'c:\ham.bin', 'wb')
+
+#boolean: true ,false 
+#none: none
+#functions  , modules , classes
+#advance : decorators , generators , iterators , meta programming 
