9eb36e4ed24d99021eb2f85928dae6800c2b3838
rishabh0009-7 <rizzlifts2024@gmail.com>
2024-09-25 16:57:05 +0530

    Create foundation.py

---
+++ 04-foundation/foundation.py
@@ -0,0 +1,9 @@
+#mutable  and immutable datatypes
+#mutable- jo change ho skta hai (list,set ,dictionary,array)
+#immutable- jo change nhi ho skta (int,float,tuples,etc)
+# python mai har cheez jo hoti hai vo as an object hoti hai 
+# int immutable hai kyuki jab int  a=10 liya to ram mai 10 jaake fix hogya aur jab humne a=11 kardia na to 11 ka alag se space bana 10 walaa mai koi change nhi hua isliye ye immutable hai 
+#jab humne change kiya na to 10 ko kabhi change nhi kiya humne  new space banaya 11  rakha cvaha  aur vo 10 automatically delete hojayega thoda time baad 
+#username = rishabh
+#username= aman 
+#isme username reference hua pehle rishabh se baad mai aman se   ye refernce mai hota hai  
