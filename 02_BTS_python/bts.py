6c6cd431ffe03d58b2ca21b16e379ab7fcc05a80
rishabh0009-7 <rizzlifts2024@gmail.com>
2024-09-25 16:57:00 +0530

    Update bts.py

--- 02_BTS_python/bts.py
+++ 02_BTS_python/bts.py
@@ -5,3 +5,14 @@
 # byte code is a low level code and it is platform independent
 #byte code run faster 
 #.pyc --> compiled python ( also known as frozen binaries )
+#__pycache__ - this  system folder is created to organise frozen binaries versions deleetd updated in this folder, source code changes , python versions etc
+# this pycache is created only when we import one file from another thats why  we imported basic func from hello.py to basic.py
+
+
+
+#python virtual machine  is machine in which code loop to iterrate  byte code 
+#also known as python interpreter
+
+#byte code is not machine code 
+#standard python implementation - cpython 
+#there are other also jython. iron python,stackless,pypy
