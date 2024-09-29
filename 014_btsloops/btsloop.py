a5c8a274c76d639545e24d780afd65755e29fc07
rishabh0009-7 <rizzlifts2024@gmail.com>
2024-09-29 18:39:15 +0530

    Update btsloop.py

--- 014_btsloops/btsloop.py
+++ 014_btsloops/btsloop.py
@@ -1,2 +1,8 @@
 # behind the scene of loops in python
 # iteration tools - for , comprehension
+#iterable objects - lists ,file 
+# resp0nse - __next__
+
+
+# steps - sabse pehle na ye iteration tools jo hai vo query karta hai iterable objects se aur bhejta hai iter () method 
+
